ff295048db5a2cfa743bc9b86f5c3cb7f163f25e
pnikrat <sniffoza@gmail.com>
2015-10-12 21:34:48 +0200

    ready for heroku v2

--- wisielec.py
+++ wisielec.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-import random, string
-from flask import Flask, render_template, url_for, flash, redirect, request
+import random
+import string
+from flask import Flask, render_template, url_for, flash, redirect, request, Markup
 
-#configuration
-DEBUG = True
+DEBUG = True  # configuration
 SECRET_KEY = 'development key'
 
 app = Flask(__name__)
@@ -13,12 +12,14 @@ app.config.from_object(__name__)
 WORDS = []
 game = None
 
+
 class Hangman:
     def __init__(self):
         self.available_letters = string.ascii_lowercase
         self.used_letters = ''
         self.word_to_guess = self.select_word()
         self.number_of_guesses = 8
+        self.win = False
 
     def get_available_letters(self):
         return self.available_letters
@@ -26,15 +27,38 @@ class Hangman:
     def get_used_letters(self):
         return self.used_letters
 
+    def get_used_letters_sorted(self):
+        return ''.join(sorted(self.get_used_letters()))
+
     def get_word_to_guess(self):
         return self.word_to_guess
+
+    def get_number_of_guesses(self):
+        return self.number_of_guesses
 
     def select_word(self):
         return WORDS[random.randrange(0, len(WORDS))]
 
     def update_letters(self, user_guess):
-        if user_guess not in self.get_used_letters():
+        if user_guess in self.get_used_letters():
+            return 'You\'ve already used that letter!'
+        elif user_guess not in self.get_used_letters():
             self.available_letters = self.get_available_letters().replace(user_guess, '')
+            self.used_letters += user_guess
+            if user_guess in self.get_word_to_guess():
+                return 'Good guess!'
+            else:
+                self.number_of_guesses -= 1
+                return 'Wrong guess!'
+
+    def check_win(self):
+        self.win = True
+        for letter in self.get_word_to_guess():
+            if letter not in self.get_used_letters():
+                self.win = False
+
+        return self.win
+
 
     def display_word(self):
         word = ''
@@ -46,27 +70,34 @@ class Hangman:
         return word
 
 
-
 def import_words():
     file = open('words.txt', 'r')
     for line in file:
-        WORDS.append(line)
+        WORDS.append(line.rstrip())
     file.close()
+
 
 @app.route('/')
 def index():
-    flash('Zapraszam do nowej gry')
+    flash('Have fun!')
     return render_template('layout.html')
+
 
 @app.route('/hangman')
 def update_game_status():
     global game
-    if game == None:
+    if game is None:
         game = Hangman()
-        return render_template('game.html', gra =  game )
+        flash('Tries left: ' + str(game.get_number_of_guesses()))
+        return render_template('game.html', gra=game)
     else:
-        pass
-        #todo logika gry
+        if game.check_win():
+            flash('You won!')
+        elif not game.check_win() and game.number_of_guesses == 0:
+            flash('You lost! The word was: ' + game.get_word_to_guess())
+        flash('Tries left: ' + str(game.get_number_of_guesses()))
+        return render_template('game.html', gra=game)
+
 
 @app.route('/newgame', methods=['POST'])
 def new_game():
@@ -74,15 +105,17 @@ def new_game():
     game = None
     return redirect(url_for('update_game_status'))
 
+
 @app.route('/guess', methods=['POST'])
 def user_guess():
     global game
     if len(request.form['letter']) != 1:
-        flash('Wpisz JEDNĄ literę!')
+        flash('Enter ONLY ONE letter!')
         return redirect(url_for('update_game_status'))
     else:
-        game.update_letters(request.form['letter'])
+        flash(game.update_letters(request.form['letter']))
         return redirect(url_for('update_game_status'))
+
 
 if __name__ == '__main__':
     import_words()
